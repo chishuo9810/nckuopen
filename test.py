f8fc516609ca0b3fa15a1dd8d27a60f3ee7afb0c
chishuo9810 <91696384+chishuo9810@users.noreply.github.com>
2024-07-14 16:43:38 +0800

    Latest version
    skip><

--- test.py
+++ test.py
@@ -16,15 +16,17 @@ def show_text(event=None):
                 player_name = result['選手'].values[0]
                 size = result['衣服尺寸'].values[0]
 
-
-                display_label = tk.Label(display_window, text=f"姓名: {player_name}, 號碼: {player_number}, 尺寸: {size}", font=("Helvetica", 72))
-                # 將 "yes" 寫回到對應的單元格
+                if len(player_name) < 5 :
+                    display_label = tk.Label(display_window, text=f"姓名: {player_name}, 號碼: {player_number}, 尺寸: {size}", font=("Helvetica", 72))
+                else:
+                    display_label = tk.Label(display_window, text=f"姓名: {player_name}, 號碼: {player_number}, 尺寸: {size}", font=("Helvetica", 30))
+                # 將 "v" 寫回到對應的單元格
                 df.loc[df['選手證號碼'] == int(number), '註記'] = "v"
                 df.to_excel(file_path, index=False, engine='openpyxl')
 
                 
             else:
-                display_label = tk.Label(display_window, text="找不到對應的選手信息", font=("Helvetica", 24))
+                display_label = tk.Label(display_window, text="找不到對應的選手信息", font=("Helvetica", 72))
             display_label.pack(pady=5)
             display_window.after(45000, display_label.destroy)  # 30秒後自動刪除標籤
         except (IndexError, ValueError):
